f9c29e477fbdf75754a9b6e91e006de3709b06a1
David John <david.john@mail.utoronto.ca>
2021-04-18 09:42:02 -0400

    Figured out how to gain the latitude and longitude of the ISS and added an index page to the application.

--- app.js
+++ app.js
@@ -6,11 +6,35 @@ const https = require("https");
 
 const app = express();
 
-app.user(express.static("public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const url = "https://api.wheretheiss.at/v1/satellites/25544";
+
+
+
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const apiData = JSON.parse(data);
+      const latitude = apiData.latitude;
+      const longitude = apiData.longitude;
+      console.log(apiData);
+
+      res.write("<p>The current latitude and longitude of the ISS is:<p>");
+      res.write("<p>Latitude: " + latitude + "<p>");
+      res.write("<p>Longitude: " + longitude + "<p>");
+
+      res.send();
+    });
+  });
+});
 
 
 
@@ -31,7 +55,9 @@ app.get("/", function (req, res) {
 
 
 
-
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000.");
+});
 
 
 //
