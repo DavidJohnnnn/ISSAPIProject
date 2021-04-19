bd4f82af1b0689ebd531f851f8672e58a4566bbe
David John <david.john@mail.utoronto.ca>
2021-04-19 14:29:43 -0400

    Made a lot of changed and added functionality to the app

--- app.js
+++ app.js
@@ -1,108 +1,63 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const async = require("async"); // This is required to do multiple API requests at the same time. Make sure it is installed.
+const variables = require("./importantVariables.js");
 
 const https = require("https");
 
-const app = express();
-
-var requests = [{
-  url: "https://api.wheretheiss.at/v1/satellites/25544"
-}, {
-  url: "https://api.kanye.rest/"
-}];
+let app = express();
+app.set('view engine', 'ejs');  // Now using EJS to help render the application.
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get("/", function (req, res) {
-  const url = "https://api.wheretheiss.at/v1/satellites/25544";
-
-  https.get(url, function (response) {
-    console.log(response.statusCode);
-    response.on("data", function (data) {
-      const apiData = JSON.parse(data);
-      const latitude = apiData.latitude;
-      const longitude = apiData.longitude;
-
-      console.log(apiData);
-
-      res.write("<p>The current latitude and longitude of the ISS is:<p>");
-      res.write("<p>Latitude: " + latitude + "<p>");
-      res.write("<p>Longitude: " + longitude + "<p>");
-      
-    });
-  });
+  console.log(variables.GeocodeAPIKey, variables.googleAPIKey);
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-  const urls = [
-    "https://api.wheretheiss.at/v1/satellites/25544",
-    "https://api.kanye.rest/"
-  ];
-
-  for (i = 0; i < 2; i++) {
-
-    https.get(urls[i], function (response) {
-      console.log(response.statusCode);
-      if (i === 0) {
-        response.on("data", function (data) {
-          const apiData = JSON.parse(data);
-          const latitude = apiData.latitude;
-          const longitude = apiData.longitude;
-
-          console.log(apiData);
-
-          res.write("<p>The current latitude and longitude of the ISS is:<p>");
-          res.write("<p>Latitude: " + latitude + "<p>");
-          res.write("<p>Longitude: " + longitude + "<p>");
-
-          res.send();
-
-        });
-    }
-      if (i === 1) {
-        response.on("data", function (data) {
-          const apiData = JSON.parse(data);
-          const latitude = apiData.quote;
-          //const longitude = apiData.longitude;
-
-          console.log(apiData);
-
-          res.write("<p>The current latitude and longitude of the ISS is:<p>");
-          res.write("<p>Latitude: " + latitude + "<p>");
-          //res.write("<p>Longitude: " + longitude + "<p>");
-
-          res.send();
-
-        });
-      }
-    });
-  }
-
-/*
-  https.get(urlNASA, function (response) {
+  https.get("https://api.wheretheiss.at/v1/satellites/25544", function (response) { // First make an https request to the Where is the ISS API.
     console.log(response.statusCode);
+    if (response.statusCode === 200) {  // If the request was successful.
+      response.on("data", function (data) {
+        const apiDataISS = JSON.parse(data);
+        const latitude = apiDataISS.latitude;
+        const longitude = apiDataISS.longitude;
 
-    response.on("data", function (data) {
-      const apiData = JSON.parse(data);
-      const latitude = apiData.quote;
-      //const longitude = apiData.longitude;
+        console.log(apiDataISS);
 
-      console.log(apiData);
+        geocodeURL = "https://www.mapquestapi.com/geocoding/v1/reverse?key=" + variables.GeocodeAPIKey + "&location=" + latitude + "%2C" + longitude + "&outFormat=json&thumbMaps=false"; // // replace variables.GeocodeAPIKey with your own MapQuest API key.
+        console.log(geocodeURL);
 
-      res.write("<p>The current latitude and longitude of the ISS is:<p>");
-      res.write("<p>Latitude: " + latitude + "<p>");
-      //res.write("<p>Longitude: " + longitude + "<p>");
+        https.get(geocodeURL, function (resp) {  // Make a request to the Geocoding api.
+          console.log("The Geocode status is " + resp.statusCode);
+          if (resp.statusCode === 200) {
+            resp.on("data", function (geodata) {
+              const apiDataGeocode = JSON.parse(geodata);
+              console.log(apiDataGeocode.results[0].locations[0]);
+              const country = apiDataGeocode.results[0].locations[0].adminArea1;
 
-      res.send();
+              const mapURL = "https://www.google.com/maps/embed/v1/place?key=" + variables.googleAPIKey + "&q=" + latitude + "%2C" + longitude + "&zoom=4"; // replace variables.googleAPIKey with your own Google API key.
 
-    });
+              res.render('index', {lat: latitude, long: longitude, ctry: country, mapLink: mapURL});
+            });
+          } else {
+            console.log("Error: the Kanye api did not provide the information properly");
+          }
+        });
+
+      });
+      /*
+      https.get("https://api.wheretheiss.at/v1/satellites/25544", function (response) {
+
+      });
+      */
+    } else {
+      console.log("Error: the ISS api did not provide the information properly");
+    }
   });
-*/
-
 });
 
 
