5615617d247640fe3c7df5d3ff4ca102833074cb
David John <david.john@mail.utoronto.ca>
2021-04-22 10:21:42 -0400

    Added a link that will plot the points recorded onto a separate link

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('view engine', 'ejs');  // Now using EJS to help render the application.
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 
+var coordinates = [];
 
 app.get("/", positionOfTheISS);
 
@@ -27,12 +28,30 @@ function positionOfTheISS (req, res) {
         const apiDataISS = JSON.parse(data);
         const latitude = apiDataISS.latitude;
         const longitude = apiDataISS.longitude;
+        coordinates.push([latitude, longitude]);
 
         //console.log(apiDataISS);
 
-        const mapURL = "https://www.google.com/maps/embed/v1/place?key=" + variables.googleAPIKey + "&zoom=4&q=" + latitude + "%2C" + longitude; // replace variables.googleAPIKey with your own Google API key.
+        var mapURL = "https://www.google.com/maps/embed/v1/place?key=" + variables.googleAPIKey + "&zoom=4&q=" + latitude + "%2C" + longitude; // replace variables.googleAPIKey with your own Google API key.
 
-        res.render('index', {lat: latitude, long: longitude, mapLink: mapURL});
+        var mapURL = "https://www.google.com/maps/embed/v1/place?key=" + variables.googleAPIKey + "&zoom=4&q=" + latitude + "%2C" + longitude; // replace variables.googleAPIKey with your own Google API key.
+
+        res.render('index', {lat: latitude, long: longitude, mapLink: mapURL, ISSCoordinates: coordinates});
+
+        var multipointURL = "https://www.google.com/maps/dir/";
+
+        for (var i = 0; i < coordinates.length; i++) {
+          if (i === coordinates.length - 1) {
+            multipointURL = multipointURL + coordinates[i][0] + "," + coordinates[i][1] + "//@" + coordinates[i][0] + "," + coordinates[i][1] + ",4z";
+          } else {
+            multipointURL = multipointURL + coordinates[i][0] + "," + coordinates[i][1] + "/";
+          }
+        }
+
+        console.log(multipointURL);
+
+        -51.799,105.927/-51.799,106.126/-51.799,106.225/-51.799,106.325//@-51.799,106.325&zoom=4";
+
 
       });
     } else {
