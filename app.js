89080f2316d575a01fb5e04b1722c46bf3ddab76
David John <david.john@mail.utoronto.ca>
2021-04-22 09:15:40 -0400

    Removed redundant HTTP request to mapquest

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ app.set('view engine', 'ejs');  // Now using EJS to help render the application.
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 
+
 app.get("/", positionOfTheISS);
 
-app.post("/", positionOfTheISS);
+app.post("/", function (req, res) {
+  res.redirect("/");
+});
 
 function positionOfTheISS (req, res) {
   https.get("https://api.wheretheiss.at/v1/satellites/25544", function (response) { // First make an https request to the Where is the ISS API.
@@ -25,34 +28,13 @@ function positionOfTheISS (req, res) {
         const latitude = apiDataISS.latitude;
         const longitude = apiDataISS.longitude;
 
-        console.log(apiDataISS);
+        //console.log(apiDataISS);
 
-        geocodeURL = "https://www.mapquestapi.com/geocoding/v1/reverse?key=" + variables.GeocodeAPIKey + "&location=" + latitude + "%2C" + longitude + "&outFormat=json&thumbMaps=false"; // // replace variables.GeocodeAPIKey with your own MapQuest API key.
-        console.log(geocodeURL);
+        const mapURL = "https://www.google.com/maps/embed/v1/place?key=" + variables.googleAPIKey + "&zoom=4&q=" + latitude + "%2C" + longitude; // replace variables.googleAPIKey with your own Google API key.
 
-        https.get(geocodeURL, function (resp) {  // Make a request to the Geocoding api.
-          console.log("The Geocode status is " + resp.statusCode);
-          if (resp.statusCode === 200) {
-            resp.on("data", function (geodata) {
-              const apiDataGeocode = JSON.parse(geodata);
-              console.log(apiDataGeocode.results[0].locations[0]);
-              const country = apiDataGeocode.results[0].locations[0].adminArea1;
-
-              const mapURL = "https://www.google.com/maps/embed/v1/place?key=" + variables.googleAPIKey + "&q=" + latitude + "%2C" + longitude + "&zoom=4"; // replace variables.googleAPIKey with your own Google API key.
-
-              res.render('index', {lat: latitude, long: longitude, ctry: country, mapLink: mapURL});
-            });
-          } else {
-            console.log("Error: the embedded Google api did not provide the information properly");
-          }
-        });
+        res.render('index', {lat: latitude, long: longitude, mapLink: mapURL});
 
       });
-      /*
-      https.get("https://api.wheretheiss.at/v1/satellites/25544", function (response) {
-
-      });
-      */
     } else {
       console.log("Error: the ISS api did not provide the information properly");
     }
